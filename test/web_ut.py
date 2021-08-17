ccee5c03f061c19d8547dd054b61f5fd1048e400
suehuijie <77597997+suehuijie@users.noreply.github.com>
2021-08-17 14:49:11 +0800

    Update web_ut.py

--- test/web_ut.py
+++ test/web_ut.py
@@ -32,29 +32,30 @@ class ISelenium(unittest.TestCase):
             using_headless = os.environ["using_headless"]
         except KeyError:
             using_headless = None
-            print('没有配置环境变量 using_headless, 按照有界面方式运行自动化测试')
+            print(u'没有配置环境变量 using_headless, 按照有界面方式运行自动化测试')
 
         chrome_options = Options()
         if using_headless is not None and using_headless.lower() == 'true':
-            print('使用无界面方式运行')
+            print(u'使用无界面方式运行')
             chrome_options.add_argument("--headless")
+        chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
 
         self.driver = webdriver.Chrome(executable_path=config.get('driver', 'chrome_driver'),
                                        options=chrome_options)
 
-    @allure.story('Test key word 今日头条')
+    @allure.story(u'Test key word 今日头条')
     def test_webui_1(self):
         """ 测试用例1，验证'今日头条'关键词在百度上的搜索结果
         """
 
-        self._test_baidu('今日头条', 'test_webui_1')
+        self._test_baidu(u'今日头条', 'test_webui_1')
 
-    @allure.story('Test key word 王者荣耀')
+    @allure.story(u'Test key word 王者荣耀')
     def test_webui_2(self):
         """ 测试用例2， 验证'王者荣耀'关键词在百度上的搜索结果
         """
 
-        self._test_baidu('王者荣耀', 'test_webui_2')
+        self._test_baidu(u'王者荣耀', 'test_webui_2')
 
     def _test_baidu(self, search_keyword, testcase_name):
         """ 测试百度搜索子函数
@@ -64,12 +65,14 @@ class ISelenium(unittest.TestCase):
         """
 
         self.driver.get("https://www.baidu.com")
-        print('打开浏览器，访问 www.baidu.com .')
+        print(u'打开浏览器，访问 www.baidu.com .')
         time.sleep(5)
-        assert f'百度一下' in self.driver.title
+        assert u'百度一下' in self.driver.title
 
         elem = self.driver.find_element_by_name("wd")
-        elem.send_keys(f'{search_keyword}{Keys.RETURN}')
-        print(f'搜索关键词~{search_keyword}')
+        elem.send_keys(search_keyword)
+        time.sleep(3)
+        elem.send_keys(webdriver.common.keys.Keys.RETURN)
+        # print(f"搜索关键词~{search_keyword}")
         time.sleep(5)
-        self.assertTrue(f'{search_keyword}' in self.driver.title, msg=f'{testcase_name}校验点 pass')
+        self.assertTrue(search_keyword in self.driver.title, msg=testcase_name + u"校验点 pass")
